d83b5786e25815138626e406a611887d64fae875
Bernton <nielsen.bernt@hotmail.com>
2023-09-26 15:31:09 +0200

    update go SDK

--- Scripts/generate-sdks.sh
+++ Scripts/generate-sdks.sh
@@ -2,7 +2,7 @@
 
 PUBSUB_PATH="$(dirname $(pwd))"
 
-INPUT_FILE=/$PUBSUB_PATH/bcc-pubsub/src/BccCode.Pubsub.Api/docs/swagger.json
+INPUT_FILE=/$PUBSUB_PATH/bcc-pubsub/src/BccCode.PubSub.Api/docs/swagger.json
 OUTPUT_DIR=/$(pwd)/pubsubsdk/model
 
 /$(pwd)/Scripts/generate-sdk.sh $INPUT_FILE $OUTPUT_DIR go "packageName=model" models
