8d9825320d898e3047a919bd7e4529105e9b7d92
Reng van Oord <reng.vanoord@bcc.no>
2023-10-03 10:41:04 +0200

    Added delete subscription endpoint

--- pubsubsdk/subscriptions.go
+++ pubsubsdk/subscriptions.go
@@ -3,6 +3,7 @@ package pubsubsdk
 import (
 	"context"
 	"net/http"
+	"net/url"
 
 	"github.com/bcc-code/bcc-pubsub-go-sdk/pubsubsdk/model"
 )
@@ -11,4 +12,8 @@ const SubscriptionUrl = "Subscriptions"
 
 func (c *Client) Subscribe(ctx context.Context, subscribeData model.SubscribeRequestData) error {
 	return c.Request(ctx, http.MethodPost, c.URL(SubscriptionUrl), subscribeData, nil)
+}
+
+func (c *Client) DeleteSubscription(ctx context.Context, typeName string, subscriptionId string) error {
+	return c.Request(ctx, http.MethodDelete, c.URL(SubscriptionUrl+"/"+typeName+"?subscriptionId="+url.QueryEscape(subscriptionId)), nil, nil)
 }
